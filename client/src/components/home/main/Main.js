e7b5583dac2078a61d18d6f76a688f8d3f6181d1
Aslam Parvej <aslamparvej2@gmail.com>
2022-01-18 00:23:53 +0530

    modify searchbar

--- client/src/components/home/main/Main.js
+++ client/src/components/home/main/Main.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SearchBar from "./SearchBar/SearchBar";
+import Card from "../../UI/Card/Card";
 import classes from "./Main.module.css";
 
 const Main = ()=>{
--- client/src/components/home/main/SearchBar/SearchBar.js
+++ client/src/components/home/main/SearchBar/SearchBar.js
@@ -1,13 +1,26 @@
-import React from "react";
-import Button from "../../../UI/Button/Button";
+import React, { useState } from "react";
+// import Button from "../../../UI/Button/Button";
 
 import classes from "./SearchBar.module.css";
 
 const SearchBar = () => {
+  const [searchData, setSearchData] = useState("");
+
+
+  const onSubmitHandler = (e)=>{
+    e.preventDefault();
+    console.log('You clicked submit.');
+    console.log('You clicked submit.', searchData);
+
+  }
+  const onChangeHandler = (e)=>{
+    setSearchData(e.target.value);
+  }
   return (
     <div className={classes.searchBar}>
-      <form>
-        <input type="text" placeholder="Search Images..."/>
+      <form onSubmit={onSubmitHandler}>
+        <input type="text" onChange={onChangeHandler} placeholder="Search Images..."/>
+        {/* <Button type="submit">Submit</Button> */}
       </form>
     </div>
   );
